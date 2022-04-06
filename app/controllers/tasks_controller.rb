bd0c0ae6d753e8c85546bce2d2787750919e5d0a
tokopo <94882217+tokopo@users.noreply.github.com>
2022-04-06 18:57:23 +0900

    メッセージ

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,6 @@
+class TasksController < ApplicationController
+  before_action :logged_in_user, only:[:edit, :update, :destroy]
+
+  def index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'tasks/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get 'sessions/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
